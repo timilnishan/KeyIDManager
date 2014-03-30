3990aa6a2ca5becbf7c95cbf29e283b54ce428f9
design_offshore <design_offshore@design-offshores-Mac-mini.local>
2014-03-30 13:29:25 +0545

    Initial Commit

---
+++ KeyIdManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KeyIdManager
+//
+//  Created by design_offshore on 3/30/14.
+//  Copyright (c) 2014 Key_id_Manager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ KeyIdManagerTests/KeyIdManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  KeyIdManagerTests.m
+//  KeyIdManagerTests
+//
+//  Created by design_offshore on 3/30/14.
+//  Copyright (c) 2014 Key_id_Manager. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KeyIdManagerTests : XCTestCase
+
+@end
+
+@implementation KeyIdManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
